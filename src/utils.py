8c6008cb7c6d2da859dbe86317dddac14baa3860
Nanji-Huaji <hua_ji@outlook.com>
2025-07-05 04:44:42 +0000

    vicuna模型必须要在vicuna目录下

--- src/utils.py
+++ src/utils.py
@@ -58,15 +58,15 @@ def model_zoo(args):
         "llama-68m": "llama/llama-68m",
         "llama-160m-q5-gguf": "llama/llama-160m-q5-gguf",
         "llama-160m": "llama/llama-160m",
-        "vicuna-68m-q5-gguf": "llama/vicuna-68m.Q5_K_M-gguf/vicuna-68m.Q5_K_M.gguf",
-        "vicuna-68m": "llama/vicuna-68m",
+        "vicuna-68m-q5-gguf": "vicuna/vicuna-68m.Q5_K_M-gguf/vicuna-68m.Q5_K_M.gguf",
+        "vicuna-68m": "vicuna/vicuna-68m",
         "vicuna-7b-v1.5": "vicuna-7b-v1.5还没部署",
         "vicuna-7b-v1.3": "vicuna-7b-v1.3还没部署",
         "llama-2-7b-chat": "llama/Llama-2-7b-chat-hf",
         "llama-68m-chat-q5-gguf": "llama/llama-68m-gguf-series/llama-68m-chat-v1.q5_k_m.gguf",
         "llama-3.2-1b": "llama/llama-3.2-1b",
         "llama-2-13b": "llama/Llama-2-13b-hf",
-        "tiny-vicuna-1b": "llama/tiny-vicuna-1b",
+        "tiny-vicuna-1b": "vicuna/tiny-vicuna-1b",
     }
 
     args.vocab_size = vocab_size[args.draft_model]
